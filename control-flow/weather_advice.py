9af5de4af0ddecc0c9717c1458b253e68f3f0034
bhubezyon <bhubezyon@gmail.com>
2025-07-17 09:36:33 -0700

    fix errors

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -2,13 +2,13 @@
 # Ask the user to input the current from a predefinedset of conditions:
 #What's the weather like today? (sunny, rainy, cold)
 # Based on the input, provide advice on what to wear.
-def get_weather_advice() :
+def get_weather_advice():
     weather = input("What's the weather like today? (sunny/rainy/cold): ") 
-    if weather == "sunny" :
+    if weather == "sunny":
         print("Wear a t-shirt and sunglasses. ")
-    elif weather == "rainy" :
+    elif weather == "rainy":
         print("Don't forget your umbrella and a raincoat. ")
-    elif weather == "cold" :
+    elif weather == "cold":
         print("Make sure to wear a warm coat and a scarf. ")
-    else :
+    else:
         print("Sorry, I don't have recommendations for this weather. ")
