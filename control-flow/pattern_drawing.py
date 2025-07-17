d5cb769b50f3c3228a5145cadebd5f9cf6732665
bhubezyon <bhubezyon@gmail.com>
2025-07-17 04:28:35 -0700

    Add pattern drawing script with while and nested for loop

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,6 @@
+# pattern_drawing.py
+# Enter the size of the pattern and draw a square pattern of that size.
+# Prompt the user for the size of the pattern and draw a square pattern of that size.
+def draw_square_pattern():
+    
+    
