779ff39743a298dc55da254f496b36d8122b6538
bhubezyon <bhubezyon@gmail.com>
2025-07-17 08:00:25 -0700

    Fix input prompt format to match checker

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -4,7 +4,7 @@
 def calculator():
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
-    operation = input("Choose the operation (+, -, *, /)): ")
+    operation = input("Choose the operation (+, -, *, /): ")
     match operation:
         case "add":
             result = num1 + num2
