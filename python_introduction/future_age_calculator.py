b886fbc9989f5883e7be405dcb3eb67231585c8d
bhubezyon <bhubezyon@gmail.com>
2025-07-17 13:32:09 -0700

    Fix error to fit description

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,10 +1,10 @@
 # future_age_calculator.py
 # Prompt user for current age
-current_age = int(input("Enter your current age: "))
+current_age = input("How old are you?: ")
 # Calculate future age
 future_year = 2050
-years_to_add = future_year - 2024
-future_age = 33 + years_to_add
+years_to_add =  - 2024
+future_age = current_age + years_to_add
 # Print result
 print(f"In {future_year}, you will be {future_age} years old.")
 
