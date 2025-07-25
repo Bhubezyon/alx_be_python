61496562867aaf8eceb9893b14a9adfa48976a8f
bhubezyon <bhubezyon@gmail.com>
2025-07-25 07:07:26 -0700

    Fix errors to meet criteria

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -4,9 +4,23 @@ def main():
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
     operation = input("Enter the operation (add, subtract, multiply, devide): ") .strip().lower()
-   
-    # result = perform_operation(num1, num2, operation)
 
-
+    #result = perform_operation()
+    def perform_operation(num1, num2, operation):
+        if operation == "add":
+            return num1 + num2
+        elif operation == "subtract":
+            return num1 - num2
+        elif operation == "multiply":
+            return num1 * num2
+        elif operation == "devide":
+            if num2 != 0:
+                return num1 / num2
+            else:
+                return "Error: Division by zero is not allowed."
+        else:
+            return "Error: Invalid operation."
+    result = perform_operation(num1, num2, operation)
+    print(f"Result: {result}")
     if __name__ == "__main__":
         main()
