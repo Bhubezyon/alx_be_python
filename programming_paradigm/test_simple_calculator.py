ec0fe9b7b783547d828e3698395aeae6765e7522
bhubezyon <bhubezyon@gmail.com>
2025-07-31 15:19:35 -0700

    Add more test examples

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -10,5 +10,27 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(-1, -1), -2)
-        # Add more assertions to thoroughly test the add method.
-        # Remember to write write additional test methods for subtract, and divide.    
+
+    def test_subtraction(self):
+        """ Test the subtraction method. """
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
+
+    def test_multiplication(self):
+        """ Test the multiplication method. """
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+        self.assertEqual(self.calc.multiply(0, 5), 0)
+
+    def test_division(self):
+        """ Test the division method. """
+        self.assertEqual(self.calc.divide(6, 3), 2)
+        self.assertEqual(self.calc.divide(5, 10), 2)
+        self.assertEqual(self.calc.divide(-6, -3), 2)
+
+        def test_division_by_zero(self):
+        """ Test the division method by zero. """
+        self.assertIsNone(self.calc.divide(10, 0))
+        self.assertIsNone(self.calc.divide(0, 0)) 
+
