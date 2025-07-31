7971cd1352b28bb50edc840f91766817bcbe4414
bhubezyon <bhubezyon@gmail.com>
2025-07-31 10:41:54 -0700

    Fixed errors

--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -4,7 +4,7 @@ from bank_account import BankAccount
 def __init__(self, account_owner, initial_balance=0):
     self.account_owner = account_owner
     self.balance = initial_balance
-    account = BankAccount("User",250)
+    account = BankAccount(250)
     # Example starting balance
     if len(sys.argv) < 2:
         print("Usage: python main-0.py <command> : <amount>")
