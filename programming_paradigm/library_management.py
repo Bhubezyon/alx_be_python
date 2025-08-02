28cc678f9d2e482dce3bd1f8bc440442657654ed
bhubezyon <bhubezyon@gmail.com>
2025-08-02 15:13:49 -0700

    Add library_management

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,41 @@
+class book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def __str__(self):
+        self._is_checked_out = False
+
+    def return_book(self):
+        self._is_checked_out = False
+
+    def is_available(self):
+        return not self._is_checked_out
+    
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def checkout_book(self, title):
+        for book in self.books:
+            if book.title == title and book._is_available():
+                book.check_out()
+                return book
+            print(f"Book '{title}' is not available.")
+
+    def return_book(self, title):
+        for book in self.books:
+            if book.title == title and book._is_available():
+                book.return_book()
+                return book
+        print(f"Book '{title}' was not checked out.")
+
+    def list_available_books (self):
+        for book in self.books:
+            if book.is_available():
+                print(f"Title: {book.title}, Author: {book.author}")
+ 
