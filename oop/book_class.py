e4c480815c4c18207648a7a344cfd92875600493
bhubezyon <bhubezyon@gmail.com>
2025-08-08 22:50:32 -0700

    Fix self.year

--- oop/book_class.py
+++ oop/book_class.py
@@ -7,7 +7,7 @@ class Book:
 
         self.title = title
         self.author = author
-        self.pages = year
+        self.year = year
 
     def __str__(self):
         return f"'{self.title} by {self.author}, published in {self.year}"
