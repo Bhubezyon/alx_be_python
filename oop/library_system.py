1cfe8ee3a008a6be6af8cdcaabc49c8377fb6ec0
bhubezyon <bhubezyon@gmail.com>
2025-08-10 06:13:42 -0700

    Fix errors to meet criteria

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,8 +1,10 @@
 # library_system.py
 
+def main():
+    my_library = Library()
+
 class Book:
     def __init__(self, title, author):
-        my_library = Library()
         self.title = title
         self.author = author
 
@@ -13,9 +15,13 @@ class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
-
+        
 def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
+def list_books(self):
+        for book in self.books:
+            print(book)
 
 class PrintBook(Book):
      def __init__(self, title, author, page_count):
@@ -24,11 +30,7 @@ class PrintBook(Book):
 
      def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
-     
-def list_books(self):
-        for book in self.books:
-            print(book)
-     
+
 class Library:
     def __init__(self):
         self.books = []
