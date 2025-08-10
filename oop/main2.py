26b79fe7f61e50ccec4d2fab5f30f9935c347ae1
bhubezyon <bhubezyon@gmail.com>
2025-08-10 06:53:02 -0700

    Add testing file polymorphism

---
+++ oop/main2.py
@@ -0,0 +1,10 @@
+from polymorphism_demo import Shape, Rectangle, Circle
+
+def main():
+    shape = [Rectangle(10, 5), Circle(7)]
+
+    for shape in shape:
+        print(f"The area of the {shape.__class__.__name__} is {shape.area()}")
+
+if __name__ == "__main__":
+    main()
