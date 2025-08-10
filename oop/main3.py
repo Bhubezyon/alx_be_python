84f162bd36c4e0b705549e0103c349e4b8753d45
bhubezyon <bhubezyon@gmail.com>
2025-08-10 08:38:36 -0700

    Fix errors to pass checker

--- oop/main3.py
+++ oop/main3.py
@@ -2,7 +2,7 @@ from class_static_methods_demo import Calculator
 
 def main():
     # Using the static method to add two numbers
-    sum_result = Calculator.add(5, 10)
+    sum_result = Calculator.add(10, 5)
     print(f"Sum: {sum_result}")  # Output: Sum: 15
 
     # Using the class method 
